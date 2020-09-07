1d69cbc3fd6ad920c1ab88841e32262c13abcd50
vitasyk <lifelinesecret@gmail.com>
2020-09-07 16:04:31 +0300

    footer continue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(function () {
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-        infinite: false,
+        infinite: false
 
     });
+    $('input, select').styler();
 
 });
